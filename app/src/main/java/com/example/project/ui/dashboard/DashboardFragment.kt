92389ac4120431db6946ce353aa264a289176dd8
kbr1946 <byeori1946@gmail.com>
2022-06-22 04:06:28 +0900

    upgrade

--- app/src/main/java/com/example/project/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/project/ui/dashboard/DashboardFragment.kt
@@ -1,17 +1,16 @@
 package com.example.project.ui.dashboard
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.project.R
 import com.example.project.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class DashboardFragment<ArrayBinding> : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
 
@@ -19,6 +18,7 @@ class DashboardFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @SuppressLint("ResourceType")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,6 +34,175 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        var idealTotalCal = 1900/3 // 계산된 적정 칼로리라고 가정
+        // 거식증의 적정 칼로리 기준
+        var idealTanCalMin = 237 * 4 / 3
+        var idealTanCalMax = 261 * 4 / 3
+        var idealDanCalMin = 71 * 4 / 3
+        var idealDanCalMax = 95 * 4 / 3
+        var idealGiCalMin = 53 * 9 / 3
+        var idealGiCalMax = 63 * 9 / 3
+
+
+        // 식품 정보(1g 당 탄, 단, 지 함량(g))
+        var foodInfo = arrayOf(arrayOf("쌀밥", 0.332, 0.03, 0.001), // 음식이름, 탄, 단, 지
+                                arrayOf("부대찌개", 0.066, 0.031, 0.031),
+                                arrayOf("배추김치", 0.033, 0.018, 0.008),
+                                arrayOf("미트볼", 0.129, 0.181, 0.107),
+                                arrayOf("숙주나물", 0.03, 0.022, 0.016))
+
+        binding.cal.setOnClickListener {
+//            var heightIn = binding.heightInput.text.toString()
+//            var weightIn = binding.weightInput.text.toString()
+//            var ageIn = binding.ageInput.text.toString()
+            //val gender_radioGroup
+
+            //각 음식별 입력 무게
+            var g = arrayOf(
+                binding.editTextNumberDecimal1.text.toString(),
+                binding.editTextNumberDecimal2.text.toString(),
+                binding.editTextNumberDecimal3.text.toString(),
+                binding.editTextNumberDecimal4.text.toString(),
+                binding.editTextNumberDecimal5.text.toString()
+            )
+
+            //각 음식별 탄,단,지 칼로리 정보
+            var foodInput = arrayOf(
+                arrayOf(foodInfo[0][1].toString().toFloat() * g[0].toFloat(),
+                    foodInfo[0][2].toString().toFloat() * g[0].toFloat(),
+                    foodInfo[0][3].toString().toFloat() * g[0].toFloat()),
+                arrayOf(foodInfo[1][1].toString().toFloat() * g[1].toFloat(),
+                    foodInfo[1][2].toString().toFloat() * g[1].toFloat(),
+                    foodInfo[1][3].toString().toFloat() * g[1].toFloat()),
+                arrayOf(foodInfo[2][1].toString().toFloat() * g[2].toFloat(),
+                    foodInfo[2][2].toString().toFloat() * g[2].toFloat(),
+                    foodInfo[2][3].toString().toFloat() * g[2].toFloat()),
+                arrayOf(foodInfo[3][1].toString().toFloat() * g[3].toFloat(),
+                    foodInfo[3][2].toString().toFloat() * g[3].toFloat(),
+                    foodInfo[3][3].toString().toFloat() * g[3].toFloat()),
+                arrayOf(foodInfo[4][1].toString().toFloat() * g[4].toFloat(),
+                    foodInfo[4][2].toString().toFloat() * g[4].toFloat(),
+                    foodInfo[4][3].toString().toFloat() * g[4].toFloat())
+            )
+
+            //입력된 음식의 총 칼로리
+            var totalCal = ((foodInput[0][0] + foodInput[1][0] + foodInput[2][0] + foodInput[3][0] + foodInput[4][0]) * 4
+                            +(foodInput[0][1] + foodInput[1][1] + foodInput[2][1] + foodInput[3][1] + foodInput[4][1]) * 4
+                            +(foodInput[0][2] + foodInput[1][2] + foodInput[2][2] + foodInput[3][2] + foodInput[4][2]) * 9)
+
+            // 적정 칼로리로 일괄 비율 조절
+            var fixCal = idealTotalCal / totalCal
+            var i: Int
+            var j: Int
+            for (i in 0..4) {
+                for (j in 0..2) {
+                    foodInput[i][j] *= fixCal
+                }
+                g[i] = (g[i].toFloat() * fixCal).toString()
+            }
+
+            //탄, 단, 지 각각 총 열량
+            var totalTanCal =
+                (foodInput[0][0] + foodInput[1][0] + foodInput[2][0] + foodInput[3][0] + foodInput[4][0]) * 4
+            var totalDanCal =
+                (foodInput[0][1] + foodInput[1][1] + foodInput[2][1] + foodInput[3][1] + foodInput[4][1]) * 4
+            var totalGiCal =
+                (foodInput[0][2] + foodInput[1][2] + foodInput[2][2] + foodInput[3][2] + foodInput[4][2]) * 9
+            var before_totalTanCal = totalTanCal
+            var before_totalDanCal = totalDanCal
+            var before_totalGiCal = totalGiCal
+
+
+            var k: Int
+            for(k in 0..20){
+                //탄수화물 칼로리 조절
+                if (idealTanCalMax < totalTanCal || totalTanCal < idealTanCalMin) {
+                    var max = 0;
+                    for (i in 1..4)
+                        if (foodInput[max][0] < foodInput[i][0])
+                            max = i
+                    var fixTan =
+                        ((idealTanCalMin + idealTanCalMax) / 2 - totalTanCal) / (foodInput[max][0] * 4) + 1
+                    foodInput[max][0] *= fixTan
+                    foodInput[max][1] *= fixTan
+                    foodInput[max][2] *= fixTan
+                    g[max] = (g[max].toFloat() * fixTan).toString()
+
+                    //조절 후 각 열량 확인
+                    totalTanCal =
+                        (foodInput[0][0] + foodInput[1][0] + foodInput[2][0] + foodInput[3][0] + foodInput[4][0]) * 4
+                    totalDanCal =
+                        (foodInput[0][1] + foodInput[1][1] + foodInput[2][1] + foodInput[3][1] + foodInput[4][1]) * 4
+                    totalGiCal =
+                        (foodInput[0][2] + foodInput[1][2] + foodInput[2][2] + foodInput[3][2] + foodInput[4][2]) * 9
+                }
+                //단백질 칼로리 조절
+                if (idealDanCalMax < totalDanCal || totalDanCal < idealDanCalMin) {
+                    var max = 0;
+                    for (i in 1..4)
+                        if (foodInput[max][1] < foodInput[i][1])
+                            max = i
+                    var fixDan =
+                        ((idealDanCalMin + idealDanCalMax) / 2 - totalDanCal) / (foodInput[max][1] * 4) + 1
+                    foodInput[max][0] *= fixDan
+                    foodInput[max][1] *= fixDan
+                    foodInput[max][2] *= fixDan
+                    g[max] = (g[max].toFloat() * fixDan).toString()
+
+                    //조절 후 각 열량 확인
+                    totalTanCal =
+                        (foodInput[0][0] + foodInput[1][0] + foodInput[2][0] + foodInput[3][0] + foodInput[4][0]) * 4
+                    totalDanCal =
+                        (foodInput[0][1] + foodInput[1][1] + foodInput[2][1] + foodInput[3][1] + foodInput[4][1]) * 4
+                    totalGiCal =
+                        (foodInput[0][2] + foodInput[1][2] + foodInput[2][2] + foodInput[3][2] + foodInput[4][2]) * 9
+                }
+                //지방 칼로리 조절
+                if (idealGiCalMax < totalGiCal || totalGiCal < idealGiCalMin) {
+                    var max = 0;
+                    for (i in 1..4)
+                        if (foodInput[max][2] < foodInput[i][2])
+                            max = i
+                    var fixGi =
+                        ((idealGiCalMin + idealGiCalMax) / 2 - totalGiCal) / (foodInput[max][2] * 9) + 1
+                    foodInput[max][0] *= fixGi
+                    foodInput[max][1] *= fixGi
+                    foodInput[max][2] *= fixGi
+                    g[max] = (g[max].toFloat() * fixGi).toString()
+
+                    //조절 후 각 열량 확인
+                    totalTanCal =
+                        (foodInput[0][0] + foodInput[1][0] + foodInput[2][0] + foodInput[3][0] + foodInput[4][0]) * 4
+                    totalDanCal =
+                        (foodInput[0][1] + foodInput[1][1] + foodInput[2][1] + foodInput[3][1] + foodInput[4][1]) * 4
+                    totalGiCal =
+                        (foodInput[0][2] + foodInput[1][2] + foodInput[2][2] + foodInput[3][2] + foodInput[4][2]) * 9
+                }
+            }
+
+            var fixTotalCal = totalTanCal + totalDanCal + totalGiCal
+            //식단 분석, 교정 결과 출력
+            binding.resultFood1.text = "<사용자 입력 정보>\n" +
+                                        "총 열량: " + totalCal.toInt() + "kcal\n" +
+                                        "탄수화물: " + before_totalTanCal.toInt() + "kcal\n" +
+                                        "단백질: " + before_totalDanCal.toInt() + "kcal\n" +
+                                        "지방: " + before_totalGiCal.toInt() + "kcal\n"
+
+            binding.resultFood2.text = "<권장 섭취량>\n" +
+                                        "권장 열량: " + idealTotalCal + "kcal\n" +
+                                        "탄수화물: " + idealTanCalMin + " ~ " + idealTanCalMax +"kcal\n" +
+                                        "단백질: " + idealDanCalMin + " ~ " + idealDanCalMax +"kcal\n" +
+                                        "지방: " + idealGiCalMin + " ~ " + idealGiCalMax +"kcal\n"
+
+            binding.resultFood3.text = "<식단 교정 안내>\n" +
+                                        foodInfo[0][0] + ": " + g[0].toFloat().toInt() + "g\n" +
+                                        foodInfo[1][0] + ": " + g[1].toFloat().toInt() + "g\n" +
+                                        foodInfo[2][0] + ": " + g[2].toFloat().toInt() + "g\n" +
+                                        foodInfo[3][0] + ": " + g[3].toFloat().toInt() + "g\n" +
+                                        foodInfo[4][0] + ": " + g[4].toFloat().toInt() + "g\n"
+        }
+
         return root
 
     }
--- app/src/main/java/com/example/project/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/project/ui/home/HomeFragment.kt
@@ -1,20 +1,12 @@
 package com.example.project.ui.home
 
-import android.R
-import android.content.Intent
 import android.os.Bundle
-import android.renderscript.ScriptGroup
-import android.util.Half.toFloat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import com.example.project.databinding.ActivityMainBinding
+import com.example.project.R
 import com.example.project.databinding.FragmentHomeBinding
 import kotlin.math.roundToInt
 
@@ -44,18 +36,94 @@ class HomeFragment : Fragment() {
 //            }
 //        }
 
-        val disease = arrayOf("비만", "거식증", "폭식증")
-        //val spinner: Spinner = binding.GenderM.findViewById(binding.disease)
-        
         binding.analyse.setOnClickListener{
             var heightIn = binding.heightInput.text.toString()
             var weightIn = binding.weightInput.text.toString()
             var ageIn = binding.ageInput.text.toString()
-            //val gender_radioGroup
 
-            var eat = 135.3-30.8* ageIn.toFloat() + 1.16 * (10 * weightIn.toFloat() + 934 * heightIn.toFloat()/100)
-            binding.result.text = "<섭취량 분석 결과>\n권장 섭취량 : " + eat.roundToInt() + "Kcal\n탄수화물: " + (eat*0.5/4).roundToInt() + "~" + (eat*0.55/4).roundToInt() +
-                                    "g\n단백질: " + (eat*0.15/4).roundToInt() + "~" + (eat*0.2/4).roundToInt() + "g\n지방: " + (eat*0.3/9).roundToInt() + "g이내"
+        //성별 설정
+            //var genderIn = "male"
+            var genderIn = "female"
+
+        // 질환명 설정
+            //var diseaseIn = "비만"
+            var diseaseIn = "거식증"
+            //var diseaseIn = "폭식증"
+
+        //질환 단계 설정
+        // 비만
+            //var diseaseLevelIn = "체중감소시기"
+            //var diseaseLevelIn = "체중유지시기"
+        //거식증
+            //var diseaseLevelIn = "초기"
+            var diseaseLevelIn = "체중증가시기"
+            //var diseaseLevelIn = "체중유지시기"
+        //폭식증
+            //var diseaseLevelIn = "대사율정상"
+            //var diseaseLevelIn = "대사율낮음"
+
+        //사용자 정보를 바탕으로 에너지 요구량 분석(저활동적이라고 가정)
+            var eat = 0.0 // 에너지 요구량
+            if(genderIn == "male")
+                eat = 88.5 - 69.1 * ageIn.toFloat() + 1.13 * (26.7 * weightIn.toFloat() + 903 * heightIn.toFloat()/100)
+            else if(genderIn == "female")
+                eat = 135.3 - 30.8 * ageIn.toFloat() + 1.16 * (10 * weightIn.toFloat() + 934 * heightIn.toFloat()/100)
+
+        //생애주기별 부가량
+            if(ageIn.toInt() in 3..8)
+                eat += 20
+            else if(ageIn.toInt() in 9..18)
+                eat += 25
+            else
+                eat = -1.0
+
+        //질환별 섭취량 분석
+            var tanMin = 0
+            var tanMax = 0
+            var danMin = 0
+            var danMax = 0
+            var giMin = 0
+            var giMax = 0
+
+            if(diseaseIn == "비만"){
+                if(diseaseLevelIn == "체중감소시기")
+                    eat -= 500
+                tanMin = (eat*0.50/4).roundToInt()
+                tanMax = (eat*0.60/4).roundToInt()
+                danMin = (eat*0.15/4).roundToInt()
+                danMax = (eat*0.25/4).roundToInt()
+                giMin = (eat*0.15/9).roundToInt()
+                giMax = (eat*0.25/9).roundToInt()
+            }
+            else if(diseaseIn == "거식증"){
+                if(diseaseLevelIn == "초기")
+                    eat = 35 * weightIn.toDouble() // 35kacl/kg/day
+                else if(diseaseLevelIn == "체중증가시기")
+                    eat = eat
+                    //eat += 100 // 1일당 100kcal씩 증가
+
+                else if(diseaseLevelIn == "체중유지시기")
+                    eat += 70 // 1일당 70kcal씩 증가 (총 여성 3000~4000kcal, 남성 4000~4500kcal가 되도록)
+                tanMin = (eat*0.50/4).roundToInt()
+                tanMax = (eat*0.55/4).roundToInt()
+                danMin = (eat*0.15/4).roundToInt()
+                danMax = (eat*0.20/4).roundToInt()
+                giMin = (eat*0.25/9).roundToInt()
+                giMax = (eat*0.30/9).roundToInt()
+            }
+            else if(diseaseIn == "폭식증"){
+                if(diseaseLevelIn == "대사율낮음") {
+                    eat = 1500.0
+                    eat += 150 // 1일당 150kcal씩 증가
+                }
+            }
+
+
+            binding.result.text = "<섭취량 분석 결과>\n" +
+                                    "1일 권장 섭취량 : " + eat.roundToInt() + "Kcal\n" +
+                                    "탄수화물: " + tanMin + "~" + tanMax + "g\n" +
+                                    "단백질: " + danMin + "~" + danMax + "g\n" +
+                                    "지방: " + giMin + "~" + giMax + "g\n"
         }
 
 
